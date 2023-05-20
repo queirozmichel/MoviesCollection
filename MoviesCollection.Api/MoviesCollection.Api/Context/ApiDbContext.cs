2f6ac4602a365dfa419d346041aba2004b2d570c
Michel Antunes <michelantunezqueiroz@gmail.com>
2023-05-20 16:14:30 -0300

    Criação classe de contexto da api.

---
+++ MoviesCollection.Api/MoviesCollection.Api/Context/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesCollection.Api.Models;
+
+namespace MoviesCollection.Api.Context
+{
+  public class ApiDbContext :DbContext
+  {
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Language> Languages { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<ParentalRating> ParentalRatings { get; set; }
+  }
+}
