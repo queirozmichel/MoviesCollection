5fa062d1a713ae42daa5f1a92e7d526eb01b13d0
Michel Antunes <michelantunezqueiroz@gmail.com>
2023-05-24 11:55:22 -0300

    Resolvido o problema da referência cíclica.

--- MoviesCollection.Api/MoviesCollection.Api/Program.cs
+++ MoviesCollection.Api/MoviesCollection.Api/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using MoviesCollection.Api.Context;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args); //Equivalente ao ConfigureServices()
 
 // Add services to the container.
 string? mySqlServerConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlServer(mySqlServerConnection));
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
